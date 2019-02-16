470d0fb937b95e95ca51bfa688a8500e1f458871
Matt Harrison <matthewdh@gmail.com>
2019-02-16 18:46:05 -0500

    sorting book/comic/movie list ajax endpoint response.

--- projects/list/get.php
+++ projects/list/get.php
@@ -1,11 +1,13 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
 include($_SERVER['DOCUMENT_ROOT'] . '/includes/query.php');
 
 $where    = ($_REQUEST['type']) ? ' WHERE type = "' . $_REQUEST['type'] . '"' : '';
-$query    = 'SELECT * from list' . $where . ' ORDER BY id';
+$query    = 'SELECT * from list' . $where . ' ORDER BY date ASC';
 $response = select($query);
 $data     = array('items' => $response);
 $json     = json_encode($data);
 
-header('Content-Type: application/json');
 echo $json;
