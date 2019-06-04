f07d86f028019e68be6cd0c4d7babd6459f1ab5d
Matt Harrison <matthew.harrison@homes.com>
2019-06-04 09:31:09 -0400

    adding React JS build files to version control.

---
+++ games/js/slider/precache-manifest.46711cad92981c031cb09caa9f583d4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "743774b10d8b58dccc14",
+    "url": "/games/js/slider/static/js/runtime~main.0470c34e.js"
+  },
+  {
+    "revision": "98f824f878e860fe45dc",
+    "url": "/games/js/slider/static/js/main.4223d3fa.chunk.js"
+  },
+  {
+    "revision": "fc1a04f5442fc70ce71f",
+    "url": "/games/js/slider/static/js/2.f9075ab4.chunk.js"
+  },
+  {
+    "revision": "98f824f878e860fe45dc",
+    "url": "/games/js/slider/static/css/main.0c7a270e.chunk.css"
+  },
+  {
+    "revision": "193f245c7b16e4d4281f2155c4ae604b",
+    "url": "/games/js/slider/index.html"
+  }
+];
---
+++ projects/react/build/precache-manifest.932a4b9f5f6d301d3095eae8873c4e84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01313613e22ccebe8888",
+    "url": "/projects/react/static/js/runtime~main.01313613.js"
+  },
+  {
+    "revision": "2afb553bd35af64240e0",
+    "url": "/projects/react/static/js/main.2afb553b.chunk.js"
+  },
+  {
+    "revision": "5a330d4bcf9342a6f170",
+    "url": "/projects/react/static/js/1.5a330d4b.chunk.js"
+  },
+  {
+    "revision": "2afb553bd35af64240e0",
+    "url": "/projects/react/static/css/main.e24ceff6.chunk.css"
+  },
+  {
+    "revision": "6409044d613815d72ece9aaaf728d3c3",
+    "url": "/projects/react/index.html"
+  }
+];
