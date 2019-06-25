ac02a7a7889c73f84189ba7514ec29e823ed08ff
Matt Harrison <matthew.harrison@homes.com>
2019-06-25 17:15:46 -0400

    organized where conditions. simplified querystring parsing method. filtered filter properties of data response.

--- api/longbox/query.php
+++ api/longbox/query.php
@@ -22,7 +22,7 @@ function getContributors($filters = []) {
 
   return [
     'count'   => count($contributors),
-    'filters' => $filters,
+    'filters' => $filters['contributors'],
     'query'   => $query,
     'results' => $contributors
   ];
@@ -64,8 +64,8 @@ function getData() {
 
 function getIssues($filters = []) {
   $where   = getWhereIssues($filters);
-  $isDesc  = is_null($filters['is_desc']) ? 'ASC' : 'DESC';
-  $orderBy = is_null($filters['order_by']) ? '' : 'ORDER BY ' . $filters['order_by'] . ' ' . $isDesc;
+  $isDesc  = is_null($filters['issues']['is_desc']) ? 'ASC' : 'DESC';
+  $orderBy = is_null($filters['issues']['order_by']) ? '' : 'ORDER BY ' . $filters['issues']['order_by'] . ' ' . $isDesc;
   $query   =
     "SELECT
       issues.id,
@@ -95,204 +95,212 @@ function getIssues($filters = []) {
 
   return [
     'count'   => count($issues),
-    'filters' => $filters,
+    'filters' => $filters['issues'],
     'query'   => $query,
     'results' => $issues
   ];
 }
 
 function getWhereContributors($filters = []) {
-  $delimiter          = is_null($filters['delimiter']) ? 'AND' : " {$filters['delimiter']} ";
-  $whereContributors  = '';
-  $contributorFilters = [];
+  $delimiter       = is_null($filters['delimiter']) ? 'AND' : " {$filters['delimiter']} ";
+  $whereClause     = '';
+  $whereConditions = [];
 
+  // Foreign Key filters
   if (!is_null($filters['contributors']['creator'])) {
     if (strpos($filters['contributors']['creator'], ',') > -1) {
-      $creators             = explode(',', $filters['contributors']['creator']);
-      $contributorFilters[] = "(creators.name LIKE '%" . implode("%' OR creators.name LIKE '%", $creators) . "%')";
+      $creators          = explode(',', $filters['contributors']['creator']);
+      $whereConditions[] = "(creators.name LIKE '%" . implode("%' OR creators.name LIKE '%", $creators) . "%')";
     } else {
-      $contributorFilters[] = "creators.name LIKE '%" . $filters['contributors']['creator'] . "%'";
-    }
-  }
-
-  if (!is_null($filters['contributors']['cover'])) {
-    if (strpos($filters['contributors']['cover'], ',') > -1) {
-      $creators             = explode(',', $filters['contributors']['cover']);
-      $contributorFilters[] = "creator_types.name = 'cover' AND (creators.name LIKE '%" . implode("' OR creators.name LIKE '%", $creators) . "%')";
-    } else {
-      $contributorFilters[] = "creator_types.name = 'cover' AND creators.name LIKE '%" . $filters['contributors']['cover'] . "%'";
-    }
-  }
-
-  if (!is_null($filters['contributors']['writer'])) {
-    if (strpos($filters['contributors']['writer'], ',') > -1) {
-      $creatorTypeIds       = explode(',', $filters['contributors']['writer']);
-      $contributorFilters[] = "creator_types.name = 'writer' AND (creators.name LIKE '%" . implode("' OR creators.name LIKE '%", $creatorTypeIds) . "%')";
-    } else {
-      $contributorFilters[] = "creator_types.name = 'writer' AND creators.name LIKE '%" . $filters['contributors']['writer'] . "%'";
-    }
-  }
-
-  if (!is_null($filters['contributors']['interior'])) {
-    if (strpos($filters['contributors']['interior'], ',') > -1) {
-      $creators             = explode(',', $filters['contributors']['interior']);
-      $contributorFilters[] = "creator_types.name = 'interior' AND (creators.name LIKE '%" . implode("' OR creators.name LIKE '%", $creators) . "%')";
-    } else {
-      $contributorFilters[] = "creator_types.name = 'interior' AND creators.name LIKE '%" . $filters['contributors']['interior'] . "%'";
-    }
-  }
-
-  if (!is_null($filters['contributors']['creator_id'])) {
-    if (strpos($filters['contributors']['creator_id'], ',') > -1) {
-      $creatorIds           = explode(',', $filters['contributors']['creator_id']);
-      $contributorFilters[] = "(creators.id = '" . implode("' OR creators.id = '", $creatorIds) . "')";
-    } else {
-      $contributorFilters[] = "creators.id = '" . $filters['contributors']['creator_id'] . "'";
+      $whereConditions[] = "creators.name LIKE '%" . $filters['contributors']['creator'] . "%'";
     }
   }
 
   if (!is_null($filters['contributors']['creator_type'])) {
     if (strpos($filters['contributors']['creator_type'], ',') > -1) {
-      $creatorTypes         = explode(',', $filters['contributors']['creator_type']);
-      $contributorFilters[] = "(creator_types.name = '" . implode("' OR creator_types.name = '", $creatorTypes) . "')";
+      $creatorTypes      = explode(',', $filters['contributors']['creator_type']);
+      $whereConditions[] = "(creator_types.name = '" . implode("' OR creator_types.name = '", $creatorTypes) . "')";
     } else {
-      $contributorFilters[] = "creator_types.name = '" . $filters['contributors']['creator_type'] . "'";
+      $whereConditions[] = "creator_types.name = '" . $filters['contributors']['creator_type'] . "'";
+    }
+  }
+
+  // ID filters
+  if (!is_null($filters['contributors']['creator_id'])) {
+    if (strpos($filters['contributors']['creator_id'], ',') > -1) {
+      $creatorIds        = explode(',', $filters['contributors']['creator_id']);
+      $whereConditions[] = "(creators.id = '" . implode("' OR creators.id = '", $creatorIds) . "')";
+    } else {
+      $whereConditions[] = "creators.id = '" . $filters['contributors']['creator_id'] . "'";
     }
   }
 
   if (!is_null($filters['contributors']['creator_type_id'])) {
     if (strpos($filters['contributors']['creator_type_id'], ',') > -1) {
-      $creatorTypeIds       = explode(',', $filters['contributors']['creator_type_id']);
-      $contributorFilters[] = "(creator_types.id = '" . implode("' OR creator_types.id = '", $creatorTypeIds) . "')";
+      $creatorTypeIds    = explode(',', $filters['contributors']['creator_type_id']);
+      $whereConditions[] = "(creator_types.id = '" . implode("' OR creator_types.id = '", $creatorTypeIds) . "')";
     } else {
-      $contributorFilters[] = "creator_types.id = '" . $filters['contributors']['creator_type_id'] . "'";
+      $whereConditions[] = "creator_types.id = '" . $filters['contributors']['creator_type_id'] . "'";
     }
   }
 
-  if (count($contributorFilters) > 0) {
-    $whereContributors = 'WHERE ' . implode(" {$delimiter} ", $contributorFilters);
+  // Custom filters (alaso based on Foreign Keys)
+  if (!is_null($filters['contributors']['cover'])) {
+    if (strpos($filters['contributors']['cover'], ',') > -1) {
+      $creators          = explode(',', $filters['contributors']['cover']);
+      $whereConditions[] = "creator_types.name = 'cover' AND (creators.name LIKE '%" . implode("' OR creators.name LIKE '%", $creators) . "%')";
+    } else {
+      $whereConditions[] = "creator_types.name = 'cover' AND creators.name LIKE '%" . $filters['contributors']['cover'] . "%'";
+    }
   }
 
-  return $whereContributors;
+  if (!is_null($filters['contributors']['writer'])) {
+    if (strpos($filters['contributors']['writer'], ',') > -1) {
+      $creatorTypeIds    = explode(',', $filters['contributors']['writer']);
+      $whereConditions[] = "creator_types.name = 'writer' AND (creators.name LIKE '%" . implode("' OR creators.name LIKE '%", $creatorTypeIds) . "%')";
+    } else {
+      $whereConditions[] = "creator_types.name = 'writer' AND creators.name LIKE '%" . $filters['contributors']['writer'] . "%'";
+    }
+  }
+
+  if (!is_null($filters['contributors']['interior'])) {
+    if (strpos($filters['contributors']['interior'], ',') > -1) {
+      $creators          = explode(',', $filters['contributors']['interior']);
+      $whereConditions[] = "creator_types.name = 'interior' AND (creators.name LIKE '%" . implode("' OR creators.name LIKE '%", $creators) . "%')";
+    } else {
+      $whereConditions[] = "creator_types.name = 'interior' AND creators.name LIKE '%" . $filters['contributors']['interior'] . "%'";
+    }
+  }
+
+  if (count($whereConditions) > 0) {
+    $whereClause = 'WHERE ' . implode(" {$delimiter} ", $whereConditions);
+  }
+
+  return $whereClause;
 }
 
 function getWhereIssues($filters = []) {
-  $delimiter    = is_null($filters['delimiter']) ? 'AND' : $filters['delimiter'];
-  $whereIssues  = '';
-  $issueFilters = [];
+  $delimiter       = is_null($filters['delimiter']) ? 'AND' : $filters['delimiter'];
+  $whereClause     = '';
+  $whereConditions = [];
 
+  // Foreign Key filters
   if (!is_null($filters['issues']['format'])) {
     if (strpos($filters['issues']['format'], ',') > -1) {
-      $formats        = explode(',', $filters['issues']['format']);
-      $issueFilters[] = "(formats.name = '" . implode("' OR formats.name = '", $formats) . "')";
+      $formats           = explode(',', $filters['issues']['format']);
+      $whereConditions[] = "(formats.name = '" . implode("' OR formats.name = '", $formats) . "')";
     } else {
-      $issueFilters[] = "formats.name = '" . $filters['issues']['format'] . "'";
-    }
-  }
-
-  if (!is_null($filters['issues']['format_id'])) {
-    if (strpos($filters['issues']['format_id'], ',') > -1) {
-      $formatIds      = explode(',', $filters['issues']['format_id']);
-      $issueFilters[] = "(formats.id = '" . implode("' OR formats.id = '", $formatIds) . "')";
-    } else {
-      $issueFilters[] = "formats.id = '" . $filters['issues']['format_id'] . "'";
-    }
-  }
-
-  if (!is_null($filters['issues']['issue_id'])) {
-    if (strpos($filters['issues']['issue_id'], ',') > -1) {
-      $issueIds       = explode(',', $filters['issues']['issue_id']);
-      $issueFilters[] = "(issues.id = '" . implode("' OR issues.id = '", $issueIds) . "')";
-    } else {
-      $issueFilters[] = "issues.id = '" . $filters['issues']['issue_id'] . "'";
-    }
-  }
-
-  if (!is_null($filters['issues']['is_color'])) {
-    $issueFilters[] = "is_color = '" . $filters['issues']['is_color'] . "'";
-  }
-
-  if (!is_null($filters['issues']['is_owned'])) {
-    $issueFilters[] = "is_owned = '" . $filters['issues']['is_owned'] . "'";
-  }
-
-  if (!is_null($filters['issues']['is_read'])) {
-    $issueFilters[] = "is_read = '" . $filters['issues']['is_read'] . "'";
-  }
-
-  if (!is_null($filters['issues']['number'])) {
-    if (strpos($filters['issues']['number'], ',') > -1) {
-      $numbers        = explode(',', $filters['issues']['number']);
-      $issueFilters[] = "(number = " . implode(' OR number = ', $numbers) . ")";
-    } else {
-      $issueFilters[] = "number = '" . $filters['issues']['number'] . "'";
-    }
-  }
-
-  if (!is_null($filters['issues']['notes'])) {
-    if (strpos($filters['issues']['notes'], ',') > -1) {
-      $notes          = explode(',', $filters['issues']['notes']);
-      $issueFilters[] = "(notes LIKE '%" . implode("%' OR notes LIKE '", $notes) . "%')";
-    } else {
-      $issueFilters[] = "notes LIKE '%" . $filters['issues']['notes'] . "%'";
+      $whereConditions[] = "formats.name = '" . $filters['issues']['format'] . "'";
     }
   }
 
   if (!is_null($filters['issues']['publisher'])) {
     if (strpos($filters['issues']['publisher'], ',') > -1) {
-      $publisher      = explode(',', $filters['issues']['publisher']);
-      $issueFilters[] = "(publishers.name LIKE '%" . implode("%' OR publishers.name LIKE '%", $publisher) . "%')";
+      $publisher         = explode(',', $filters['issues']['publisher']);
+      $whereConditions[] = "(publishers.name LIKE '%" . implode("%' OR publishers.name LIKE '%", $publisher) . "%')";
     } else {
-      $issueFilters[] = "publishers.name LIKE '%" . $filters['issues']['publisher'] . "%'";
-    }
-  }
-
-  if (!is_null($filters['issues']['publisher_id'])) {
-    if (strpos($filters['issues']['publisher_id'], ',') > -1) {
-      $publisherIds   = explode(',', $filters['issues']['publisher_id']);
-      $issueFilters[] = "(publishers.id = '" . implode("' OR publishers.id = '", $publisherIds) . "')";
-    } else {
-      $issueFilters[] = "publishers.id = '" . $filters['issues']['publisher_id'] . "'";
+      $whereConditions[] = "publishers.name LIKE '%" . $filters['issues']['publisher'] . "%'";
     }
   }
 
   if (!is_null($filters['issues']['title'])) {
     if (strpos($filters['issues']['title'], ',') > -1) {
-      $title          = explode(',', $filters['issues']['title']);
-      $issueFilters[] = "(titles.name LIKE '%" . implode("%' OR titles.name LIKE '%", $title) . "%')";
+      $title             = explode(',', $filters['issues']['title']);
+      $whereConditions[] = "(titles.name LIKE '%" . implode("%' OR titles.name LIKE '%", $title) . "%')";
     } else {
-      $issueFilters[] = "titles.name LIKE '%" . $filters['issues']['title'] . "%'";
+      $whereConditions[] = "titles.name LIKE '%" . $filters['issues']['title'] . "%'";
+    }
+  }
+
+  // ID filters
+  if (!is_null($filters['issues']['format_id'])) {
+    if (strpos($filters['issues']['format_id'], ',') > -1) {
+      $formatIds         = explode(',', $filters['issues']['format_id']);
+      $whereConditions[] = "(formats.id = '" . implode("' OR formats.id = '", $formatIds) . "')";
+    } else {
+      $whereConditions[] = "formats.id = '" . $filters['issues']['format_id'] . "'";
+    }
+  }
+
+  if (!is_null($filters['issues']['issue_id'])) {
+    if (strpos($filters['issues']['issue_id'], ',') > -1) {
+      $issueIds          = explode(',', $filters['issues']['issue_id']);
+      $whereConditions[] = "(issues.id = '" . implode("' OR issues.id = '", $issueIds) . "')";
+    } else {
+      $whereConditions[] = "issues.id = '" . $filters['issues']['issue_id'] . "'";
+    }
+  }
+
+  if (!is_null($filters['issues']['publisher_id'])) {
+    if (strpos($filters['issues']['publisher_id'], ',') > -1) {
+      $publisherIds      = explode(',', $filters['issues']['publisher_id']);
+      $whereConditions[] = "(publishers.id = '" . implode("' OR publishers.id = '", $publisherIds) . "')";
+    } else {
+      $whereConditions[] = "publishers.id = '" . $filters['issues']['publisher_id'] . "'";
     }
   }
 
   if (!is_null($filters['issues']['title_id'])) {
     if (strpos($filters['issues']['title_id'], ',') > -1) {
-      $titleIds = explode(',', $filters['issues']['title_id']);
-      $issueFilters[] = "(titles.id = '" . implode("' OR titles.id = '", $titleIds) . "')";
+      $titleIds          = explode(',', $filters['issues']['title_id']);
+      $whereConditions[] = "(titles.id = '" . implode("' OR titles.id = '", $titleIds) . "')";
     } else {
-      $issueFilters[] = "titles.id = '" . $filters['issues']['title_id'] . "'";
+      $whereConditions[] = "titles.id = '" . $filters['issues']['title_id'] . "'";
+    }
+  }
+
+  // Boolean filters
+  if (!is_null($filters['issues']['is_color'])) {
+    $whereConditions[] = "is_color = '" . $filters['issues']['is_color'] . "'";
+  }
+
+  if (!is_null($filters['issues']['is_owned'])) {
+    $whereConditions[] = "is_owned = '" . $filters['issues']['is_owned'] . "'";
+  }
+
+  if (!is_null($filters['issues']['is_read'])) {
+    $whereConditions[] = "is_read = '" . $filters['issues']['is_read'] . "'";
+  }
+
+  // String/Number filters
+  if (!is_null($filters['issues']['number'])) {
+    if (strpos($filters['issues']['number'], ',') > -1) {
+      $numbers           = explode(',', $filters['issues']['number']);
+      $whereConditions[] = "(number = " . implode(' OR number = ', $numbers) . ")";
+    } else {
+      $whereConditions[] = "number = '" . $filters['issues']['number'] . "'";
+    }
+  }
+
+  if (!is_null($filters['issues']['notes'])) {
+    if (strpos($filters['issues']['notes'], ',') > -1) {
+      $notes             = explode(',', $filters['issues']['notes']);
+      $whereConditions[] = "(notes LIKE '%" . implode("%' OR notes LIKE '", $notes) . "%')";
+    } else {
+      $whereConditions[] = "notes LIKE '%" . $filters['issues']['notes'] . "%'";
     }
   }
 
   if (!is_null($filters['issues']['year'])) {
     if (strpos($filters['issues']['year'], ',') > -1) {
-      $year           = explode(',', $filters['issues']['year']);
-      $issueFilters[] = "(year LIKE '%" . implode("' OR year LIKE '%", $year) . "')";
+      $year              = explode(',', $filters['issues']['year']);
+      $whereConditions[] = "(year LIKE '%" . implode("' OR year LIKE '%", $year) . "')";
     } else {
-      $issueFilters[] = "year LIKE '%" . $filters['issues']['year'] . "'";
+      $whereConditions[] = "year LIKE '%" . $filters['issues']['year'] . "'";
     }
   }
 
+  // Filter by any Issue IDs identified by Contributor query
   if (!is_null($filters['issues']['issueIds'])) {
     $issueIds       = count($filters['issues']['issueIds']) > 0 ? implode(',', $filters['issues']['issueIds']) : 'NULL';
-    $issueFilters[] = "issues.id IN ({$issueIds})";
+    $whereConditions[] = "issues.id IN ({$issueIds})";
   }
 
-  if (count($issueFilters) > 0) {
-    $whereIssues = 'WHERE ' . implode(" {$delimiter} ", $issueFilters);
+  if (count($whereConditions) > 0) {
+    $whereClause = 'WHERE ' . implode(" {$delimiter} ", $whereConditions);
   }
 
-  return $whereIssues;
+  return $whereClause;
 }
 
 function parseFilters() {
@@ -330,25 +338,23 @@ function parseFilters() {
   ];
 
   foreach ($_REQUEST as $key => $value) {
+    if (strpos($key, 'is_') > -1) {
+      $value = ($value === 'true' || $value == '1');
+    }
+
     if (in_array($key, $contributorKeys)) {
       $filters['contributors'][$key] = $value;
     } elseif (in_array($key, $issueKeys)) {
-      if (strpos($key, 'is_') > -1) {
-        $filters['issues'][$key] = ($value === 'true' || $value == '1');
-      } else {
-        $filters['issues'][$key] = $value;
-      }
+      $filters['issues'][$key] = $value;
+    } elseif ($key === 'any') {
+      $filters['contributors']['creator'] .= count($filters['contributors']['creator']) > 0 ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
+      $filters['issues']['notes']         .= count($filters['issues']['notes']) > 0         ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
+      $filters['issues']['publisher']     .= count($filters['issues']['publisher']) > 0     ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
+      $filters['issues']['title']         .= count($filters['issues']['title']) > 0         ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
+      $filters['delimiter'] = 'OR';
+    } else {
+      $filters[$key] = $value;
     }
-  }
-
-  if ($_REQUEST['any']) {
-    $filters['contributors']['creator'] .= count($filters['contributors']['creator']) > 0 ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
-    $filters['issues']['notes']         .= count($filters['issues']['notes']) > 0         ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
-    $filters['issues']['publisher']     .= count($filters['issues']['publisher']) > 0     ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
-    $filters['issues']['title']         .= count($filters['issues']['title']) > 0         ? ',' . $_REQUEST['any'] : $_REQUEST['any'];
-    $filters['delimiter'] = 'OR';
-  } elseif ($_REQUEST['delimiter']) {
-    $filters['delimiter'] = $_REQUEST['delimiter'];
   }
 
   if (count($filters['contributors']) > 0) {
