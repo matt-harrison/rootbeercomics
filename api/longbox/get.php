b32fb01337368c33f4004d5424bf960b4e9b343e
Matt Harrison <matthew.harrison@homes.com>
2019-06-26 17:36:22 -0400

    mvp for longbox app frontend.

--- api/longbox/get.php
+++ api/longbox/get.php
@@ -1,5 +1,6 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/api/longbox/query.php');
 
+header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 echo json_encode(getData());
---
+++ projects/longbox/precache-manifest.ec5b357a607f32167d18152a96d87f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9072963f2bfac27ff23fbb7a368fd723",
+    "url": "/projects/longbox/index.html"
+  },
+  {
+    "revision": "b61c88049e55a11f3981",
+    "url": "/projects/longbox/static/css/3.3ff2e0eb.chunk.css"
+  },
+  {
+    "revision": "d806f9b4d0dd3f278618",
+    "url": "/projects/longbox/static/js/2.38c4fd7c.chunk.js"
+  },
+  {
+    "revision": "b61c88049e55a11f3981",
+    "url": "/projects/longbox/static/js/3.317e975a.chunk.js"
+  },
+  {
+    "revision": "950a8fd61b3c4616bd11",
+    "url": "/projects/longbox/static/js/main.b0e6ed66.chunk.js"
+  },
+  {
+    "revision": "df6a2609dc3967933db5",
+    "url": "/projects/longbox/static/js/runtime~main.bb1f82f8.js"
+  }
+]);
