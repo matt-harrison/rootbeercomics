31fbd6ca3a47ff6ca425d50705c73131161e69cb
Matt Harrison <matthew.harrison@homes.com>
2019-09-04 08:51:55 -0400

    fixed special character display in portfolio directory.

--- projects/portfolio/includes/header.php
+++ projects/portfolio/includes/header.php
@@ -4,22 +4,18 @@ if (empty($title)) {
 }
 ?>
 <!doctype html>
-<html>
+<html lang="en">
     <head>
-        <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1"/>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
         <title><?= $title; ?> by Matt Harrison</title>
         <link href="/projects/portfolio/styles.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <div id="page" class="auto mt10 mb10 bdrBlack bgWhite">
             <div id="header" class="flex spaceBetween mb20">
-                <a href="/projects/portfolio">
+                <a href="/projects/portfolio" class="mr10">
                     <img src="/projects/portfolio/img/logo.png" alt="Matt Harrison"/>
                 </a>
-                <div>
-                    <h1><?= $section; ?> by Matt Harrison</h1>
-                </div>
-                <div>
-                    <a href="mailto:pro.matt.harrison@gmail.com">pro.matt.harrison@gmail.com</a>
-                </div>
+                <h1 class="mr10"><?= $section; ?> by Matt Harrison</h1>
+                <a href="mailto:pro.matt.harrison@gmail.com">pro.matt.harrison@gmail.com</a>
             </div>
