ae78b5a8acf701e04ed86ef0bd075959f2134875
Matt Harrison <matthewdh@gmail.com>
2020-01-19 10:51:54 -0500

    added contourtionist minicomic.

---
+++ minicomics/contourionist/index.php
@@ -0,0 +1,11 @@
+<?php
+$title = 'contourtionist';
+$img   = '/minicomics/contourtionist/img/contourtionist0.jpg';
+$desc  = 'look at you, tying yourself in knots';
+
+$book  = 'contourtionist';
+$first = 0;
+$last  = 9;
+$width = 600;
+?>
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/includes/mini.php'); ?>
